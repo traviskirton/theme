19c27cf892a69e9010f248854f4931357aa0020d
Travis <travis@createwithflow.com>
2026-01-20 14:46:37 -0800

    Add module augmentation for custom theme properties

--- dist/mantine.d.ts
+++ dist/mantine.d.ts
@@ -2,7 +2,56 @@
  * Mantine theme configuration generated from design tokens.
  */
 
-import type { MantineThemeOverride } from '@mantine/core';
+import '@mantine/core'
 
-export declare const theme: MantineThemeOverride;
-export default theme;
+declare module '@mantine/core' {
+  export interface MantineSpacing {
+    none: string
+    hair: string
+    xxxs: string
+    xxs: string
+    xxl: string
+    xxxl: string
+  }
+
+  export interface MantineTheme {
+    letterSpacing: {
+      none: string
+      xsmall: string
+      small: string
+      medium: string
+      large: string
+    }
+    iconSizes: {
+      xxsmall: string
+      xsmall: string
+      small: string
+      medium: string
+      large: string
+      xlarge: string
+      xxlarge: string
+    }
+    fontWeights: {
+      regular: number
+      medium: number
+      semibold: number
+      bold: number
+    }
+    fontFamilies: {
+      sans: string
+      mono: string
+    }
+    borderWidths: {
+      xsmall: string
+      small: string
+      medium: string
+      large: string
+      xlarge: string
+    }
+  }
+}
+
+import type { MantineThemeOverride } from '@mantine/core'
+
+export declare const theme: MantineThemeOverride
+export default theme
